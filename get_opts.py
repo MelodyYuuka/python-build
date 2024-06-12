6bf0263b8f9ca420d2f00a956cc40512a145e380
Melody_Yuuka <a876987@126.com>
2024-06-13 07:51:58 +0800

    回退

--- get_opts.py
+++ get_opts.py
@@ -3,6 +3,6 @@ import sys
 version = tuple(int(i) for i in sys.argv[1].split("."))
 
 if version >= (3, 12):
-    print("--enable-optimizations --with-lto --enable-shared --enable-bolt", end="")
+    print("--enable-optimizations --with-lto --enable-shared", end="")
 else:
     print("--enable-optimizations --with-lto --enable-shared", end="")
