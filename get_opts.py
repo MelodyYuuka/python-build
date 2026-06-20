5ac133e891cc030d568a0c943ac1ebf2b47b538b
旋律 <81250368+MelodyYuuka@users.noreply.github.com>
2026-06-20 15:23:16 +0800

    Update get_opts.py

--- get_opts.py
+++ get_opts.py
@@ -7,7 +7,7 @@ flags = [
     "--enable-optimizations",
     "--with-lto",
     *(["--enable-bolt"] if v >= Version("3.12") else []),
-    *(["--enable-experimental-jit=off"] if v >= Version("3.13") else []),
+    *(["--enable-experimental-jit=no"] if v >= Version("3.13") else []),
     *(["--with-tail-call-interp"] if v >= Version("3.14") else []),
 ]
 
