4b058cfc92e8219410ca8c1d35ca7b88fc9d98f7
旋律 <81250368+MelodyYuuka@users.noreply.github.com>
2026-06-20 14:16:56 +0800

    Add version checks for build options in get_opts.py

--- get_opts.py
+++ get_opts.py
@@ -3,6 +3,10 @@ from packaging import version
 
 now = version.parse(sys.argv[1])
 
+if now >= version.parse("3.14"):
+    print("--enable-optimizations --with-lto --enable-bolt --enable-experimental-jit=yes-off --with-tail-call-interp", end="")
+if now >= version.parse("3.13"):
+    print("--enable-optimizations --with-lto --enable-bolt --enable-experimental-jit=yes-off", end="")
 if now >= version.parse("3.12"):
     print("--enable-optimizations --with-lto --enable-bolt", end="")
 else:
