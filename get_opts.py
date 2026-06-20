954e750363eeecb93b8ed455b48510d133827bd9
旋律 <81250368+MelodyYuuka@users.noreply.github.com>
2026-06-20 14:20:27 +0800

    Fix conditional checks for version parsing

--- get_opts.py
+++ get_opts.py
@@ -5,9 +5,9 @@ now = version.parse(sys.argv[1])
 
 if now >= version.parse("3.14"):
     print("--enable-optimizations --with-lto --enable-bolt --enable-experimental-jit=yes-off --with-tail-call-interp", end="")
-if now >= version.parse("3.13"):
+elif now >= version.parse("3.13"):
     print("--enable-optimizations --with-lto --enable-bolt --enable-experimental-jit=yes-off", end="")
-if now >= version.parse("3.12"):
+elif now >= version.parse("3.12"):
     print("--enable-optimizations --with-lto --enable-bolt", end="")
 else:
     print("--enable-optimizations --with-lto", end="")
